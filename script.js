f5afffc0b45e98c4ddf086f81eb10d05479eb686
Erin Cavanagh <cavanagherinm@gmail.com>
2017-05-09 13:30:20 -0400

    fizzbuzz

---
+++ script.js
@@ -0,0 +1,13 @@
+for(vi-0; i<30; i++){
+
+  if(i% 5---0 && i % 3===0){
+  console.log("fizzbuzzz");
+}else if (i% 5===0){
+  console.log("buzz");
+}else if(i%3===0){
+  console.log("fizz")
+}else{
+  console.log(i);
+}
+
+}
